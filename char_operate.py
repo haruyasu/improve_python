16fc41a7fec6e735f5f6a35149965ce719be7f5c
haruyasu <harukun2002@gmail.com>
2018-02-23 14:33:57 -0800

    improve char operate

---
+++ char_operate.py
@@ -0,0 +1,89 @@
+# replace()
+print "Hello World!".replace("Hello", "Good")
+print "Hello World!".replace(" World!", "")
+
+# in
+print "world" in "Hello"
+print "world" in "Hello world"
+
+# startswith()
+print "Hello world".startswith("Hello")
+print "Hello world".startswith("hello")
+
+# endswidth()
+print "Hello world".endswith("world")
+print "Hello world".endswith("hello")
+
+# find()
+print "apple apple apple".find("apple")
+print "apple apple apple".find("orange")
+
+# rfing()
+print "apple apple apple".rfind("apple")
+print "apple apple apple".rfind("orange")
+
+# index()
+print "apple apple apple".index("apple")
+#print "apple apple apple".index("orange")
+
+# rindex()
+print "apple apple apple".rindex("apple")
+#print "apple apple apple".rindex("orange")
+
+# isalnum()
+print "abc123".isalnum()
+print "abc 123".isalnum()
+
+# isalpha()
+print "abc123".isalpha()
+print "abcdef".isalpha()
+
+# isdigit()
+print "123456".isdigit()
+print "abcdef".isdigit()
+
+# islower()
+print "abc".islower()
+print "ABC".islower()
+
+# isspace()
+print "  ".isspace()
+print " aa bb ".isspace()
+
+# istitle()
+print "Hello World".istitle()
+print "Hello world".istitle()
+
+# capitalize()
+print "hello world".capitalize()
+
+# swapcase()
+print "HeLLO world".swapcase()
+
+# title()
+print "hello world".title()
+
+# lower()
+print "HELLO WORLd".lower()
+
+# upper()
+print "hello world".upper()
+
+# split()
+print "apple orange banana grape".split()
+print "apple,orange,banana,grape".split(",")
+print "apple,orange,banana,grape".split(",", 2)
+
+# rsplit()
+print "apple orange banana grape".rsplit()
+print "apple,orange,banana,grape".rsplit(",")
+print "apple,orange,banana,grape".rsplit(",", 2)
+
+# splitlines()
+print "apple\norange\nbanana\ngrape".splitlines()
+
+# sorted()
+ls = ["5","2","1","4","3"]
+print "".join(sorted(ls))
+num = 4215
+print sorted([c for c in str(num) + '0000'], reverse=True)[:4]
