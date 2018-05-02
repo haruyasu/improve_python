6082019641bf86334210d18e7e1121f945bc6e51
haruyasu <harukun2002@gmail.com>
2018-05-01 20:31:54 -0700

    cording interview

--- test.py
+++ test.py
@@ -1,38 +1,24 @@
-h = 7
-w = 10
-n = 1
-# li = [[1, 8, 1], [4, 1, 5], [1, 6, 2], [2, 2, 0]]
-li = [[2, 0, 2]]
+def test(s):
+    result = False
+    if len(s) % 2 != 0:
+        for i in range(int((len(s) - 1) / 2)):
+            if s[i] == s[len(s) - 1 - i]:
+                result = True
+            else:
+                return False
+    else:
+        for i in range(int(len(s) / 2)):
+            if s[i] == s[len(s) - 1 - i]:
+                result = True
+            else:
+                return False
+    return result
 
-####
-li2 = []
-for i in range(h):
-    li_ = []
-    for j in range(w):
-        li_.append(".")
-    li2.append(li_)
-
-for i in range(n):
-    h1 = li[i][0]
-    w1 = li[i][1]
-    x1 = li[i][2]
-
-    li3 = []
-    for j in range(h):
-        for k in range(w):
-            if li2[j][k] == "#":
-                li3.append([j, k])
-    print(li3)
-
-    for j in range(h):
-        for k in range(w):
-            if 0 <= k <= 1:
-                if j == 6 or j == 5:
-                    li2[j][k] = "#"
-            # if x1 <= k < (w1 + x1):
-            #     if j == h - 1:
-            #         li2[j][k] = "#"
-
-
-for i in range(h):
-    print(li2[i])
+s = "abca"
+print(test(s))
+s = "acbbca"
+print(test(s))
+s = "abcad"
+print(test(s))
+s = "aba"
+print(test(s))
