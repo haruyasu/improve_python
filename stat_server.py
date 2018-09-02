5e6280591bf7fb47b3a4054bcb28268890df6b2e
haruyasu <harukun2002@gmail.com>
2018-09-01 21:31:09 -0400

    stat

---
+++ stat_server.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python
+import os
+import struct
+
+FORMAT = "<IIIIIIIIII"
+
+stat_data = os.stat('algorithm.py')
+packed = struct.pack(FORMAT, stat_data)
+unpacked = struct.unpack(FORMAT, packed)
+
+print(unpacked)
