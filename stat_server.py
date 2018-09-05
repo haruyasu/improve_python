9d20c092790a0ac31fec7a52e55a00a4c99be7f1
haruyasu <harukun2002@gmail.com>
2018-09-04 22:48:15 -0400

    stat

--- stat_server.py
+++ stat_server.py
@@ -5,8 +5,10 @@ import struct
 FORMAT = "<IIIIIIIIII"
 
 stat_data = os.stat('algorithm.py')
+
+print(stat_data)
+
 packed = struct.pack(FORMAT, stat_data)
 unpacked = struct.unpack(FORMAT, packed)
-
 
 print(unpacked)
